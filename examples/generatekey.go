92deb0698dd61e43dfaeb546411a375c15c3ed75
Mitos <git@novostey.com>
2021-09-27 15:18:29 +0300

    some tests and example for random key generator

--- examples/generatekey.go
+++ examples/generatekey.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	aes "github.com/gitMitos/aes"
+)
+
+func main() {
+	key, err :=  aes.RandomKey()
+	if (err!=nil){
+		panic("Fail to create random encryption key")
+	}
+
+	fmt.Println( "Key is: ", key )
+	fmt.Println(" len of key is: ", len(key))
 }
